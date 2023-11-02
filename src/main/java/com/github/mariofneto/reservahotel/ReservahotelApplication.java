fc83d5c27589b3d5d89edbcc3b7c7c52569a897f
Mário Neto <marioo.netoo3@gmail.com>
2023-11-02 20:51:48 -0300

    init

---
+++ src/main/java/com/github/mariofneto/reservahotel/ReservahotelApplication.java
@@ -0,0 +1,13 @@
+package com.github.mariofneto.reservahotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservahotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservahotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mariofneto/reservahotel/ReservahotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mariofneto.reservahotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservahotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
